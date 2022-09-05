98cea7bf480336feb1e138892c7f4b618d09e33c
Dichik <omeluan.dima@gmail.com>
2022-09-05 19:56:56 +0300

    hw2: test GET /rate

--- tests/unit/fileWriterService.test.js
+++ tests/unit/fileWriterService.test.js
@@ -3,7 +3,7 @@
 const fileWriterService = require("../../services/input_output/fileWriterService")
 
 test('Test writing to the file', async () => {
-    const data = '\'["{\"email\":\"omeluan.dima@gmail.com\"}","{\"email\":\"dima.omelyan03@gmail.com\"}"]\''
+    const data = '\'["{\\"email\\":\\"omeluan.dima@gmail.com\\"}","{\\"email\\":\\"dima.omelyan03@gmail.com\\"}"]\''
     const errorFlag = false
     try {
         await fileWriterService(data, 'check_write.json')
