e7c8de10b076fcdc520a4a1cedeab54190ed9a64
Dichik <omeluan.dima@gmail.com>
2022-09-10 11:44:29 +0300

    hw3: update controllers

---
+++ app.ts
@@ -0,0 +1,48 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import CoinmarketRateController from './controllers/rate.controller'
+import EmailController from './controllers/email.controller'
+import SubscriptionController from './controllers/subscribe.controller'
+
+require('dotenv').config()
+
+class App {
+
+    private app: any = express()
+    private port: number = 8081
+    private rateController: CoinmarketRateController;
+    private emailController: EmailController;
+    private subscriptionController: SubscriptionController;
+
+    constructor() {
+        this.app.use(bodyParser.urlencoded({ extended: true }))
+        this.app.use(bodyParser.json())
+        this.app.use(bodyParser.raw())
+        this.rateController = new CoinmarketRateController()
+        this.emailController = new EmailController()
+        this.subscriptionController = new SubscriptionController()
+    }
+
+    initRoutes() {
+        this.app.get('/rate', (request: express.Request, response: express.Response) => {
+            this.rateController.rate(request, response)
+        })
+        
+        this.app.post('/subscribe', (request: express.Request, response: express.Response) => {
+            this.subscriptionController.subscribe(request, response)
+        })
+        
+        this.app.post('/sendEmails', (request: express.Request, response: express.Response) => {
+            this.emailController.sendEmails(request, response)
+        })
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Example app listening on port ${this.port}`)
+        })
+    }
+
+}
+
+export default App
--- server.ts
+++ server.ts
@@ -1,28 +1,6 @@
-import express from 'express'
-import * as emailController from './controllers/emailController'
-import * as rateController from './controllers/rateController'
-import bodyParser from 'body-parser'
-require('dotenv').config()
+import App from "./app";
 
-const port = 8081
+let app = new App()
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(bodyParser.raw())
-
-app.get('/rate', (request: express.Request, response: express.Response) => {
-	rateController.rate(request, response)
-})
-
-app.post('/subscribe', (request: express.Request, response: express.Response) => {
-	emailController.subscribe(request, response)
-})
-
-app.post('/sendEmails', (request: express.Request, response: express.Response) => {
-	emailController.sendEmails(request, response)
-})
-
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+app.initRoutes()
+app.listen()
