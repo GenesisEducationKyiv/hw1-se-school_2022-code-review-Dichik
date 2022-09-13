59f78f2db437c980ef3dbfc913d1efcdd85b0187
Dichik <omeluan.dima@gmail.com>
2022-09-13 17:24:59 +0300

    hw4: add caching for rateService

--- src/services/rating/rateService.ts
+++ src/services/rating/rateService.ts
@@ -4,9 +4,13 @@ import RateProvider from "./rateProvider.interface"
 class RateService {
 
     private rateProvider: RateProvider;
+    private lastFetchingResult: string;
+    private lastFetchingTime: number = Date.now();
+    private CACHING_TIME: number = 300000; // TODO move to env file
 
     constructor() {
         let config: string = process.env.CRYPTO_CURRENCY_PROVIDER as string
+        this.lastFetchingResult = ''
 
         if(config === 'coinmarket') {
             this.rateProvider = new CoinmarketRateService()
@@ -16,7 +20,12 @@ class RateService {
     }
 
     async rate(): Promise<string> {
-        return await this.rateProvider.getRate('BTC', 'UAH')
+        if(Date.now() - this.lastFetchingTime < this.CACHING_TIME) {
+            return this.lastFetchingResult
+        }
+        this.lastFetchingResult = await this.rateProvider.getRate('BTC', 'UAH')
+        this.lastFetchingTime = Date.now()
+        return this.lastFetchingResult
     }
 
 }
