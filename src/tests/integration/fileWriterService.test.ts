d72bd6528bf4b8e1592bfb744150e0c00a249e5f
Dichik <omeluan.dima@gmail.com>
2022-09-25 10:06:00 +0300

    tech: fid jest.config file

--- src/tests/integration/fileWriterService.test.ts
+++ src/tests/integration/fileWriterService.test.ts
@@ -11,7 +11,7 @@ describe('Test FileWriterService', () => {
     test('Test writing to the file', async () => {
         const data = ["omeluan.dima@gmail.com","dima.omelyan03@gmail.com"]
         const stringifiedData = JSON.stringify(data)
-        const result = await fileWriterService.write(stringifiedData, 'check_write.json')
+        const result = await Promise.resolve(fileWriterService.write(stringifiedData, 'check_write.json'))
         expect(result).not.toBe(null)
     })
 
