9c36916a9fe482f7320a2f5e2315cff699a45955
Dichik <omeluan.dima@gmail.com>
2022-09-30 08:39:17 +0300

    hw6: fix communication between microservices

--- subscription-service/src/services/senders/adapters/nodemailerAdapter.ts
+++ subscription-service/src/services/senders/adapters/nodemailerAdapter.ts
@@ -1,31 +1,37 @@
-import CryptoCurrencyChain from '../../../rating/chain/cryptoCurrencyChain'
-import { EmailEntity } from '../../models/email.entity'
-import EmailAdapter from './emailAdapter.interface'
+import axios from "axios";
+import { EmailEntity } from "../../../models/email.entity";
+import EmailAdapter from "./emailAdapter.interface";
 
 class NodemailerAdapter implements EmailAdapter {
-    private providerChain: CryptoCurrencyChain
-
-    constructor() {
-        this.providerChain = new CryptoCurrencyChain()
-    }
 
     async getMailOptions(email: EmailEntity): Promise<any> {
-        const priceForBTC = await Promise.resolve(
-            this.providerChain.getCurrencyRate()
-        )
+        
+        const priceForBTC = await Promise.resolve(this.getRate()) // TODO fix issue with getting price
 
-        const mailSubject = 'BTC price in UAH'
-        const mailBody = `Price for BTC ${priceForBTC} UAH`
+		const mailSubject = 'BTC price in UAH'
+		const mailBody = `Price for BTC ${priceForBTC} UAH`
         const recipient: string = email.address
 
         const mailOptions = {
-            from: process.env.SENDER_EMAIL,
-            to: recipient,
-            subject: mailSubject,
-            text: mailBody,
-        }
+			from: process.env.SENDER_EMAIL,
+			to: recipient,
+			subject: mailSubject,
+			text: mailBody,
+		}
         return mailOptions
     }
+
+    private async getRate(): Promise<any> {
+        try {
+            const result = await Promise.resolve(axios.get(
+                'http://localhost:8081/rate'
+            ))
+            return result
+        } catch (error) {
+            throw new Error(error as string)
+        }
+    }
+
 }
 
-export default NodemailerAdapter
+export default NodemailerAdapter;
